87d9bc1fcff34bb2bf3aee280f1e3568b36fb6a8
Huffenagle <Huffenagle@Gmail.com>
2024-08-27 16:48:50 -0400

    added sprite groups and added player to group in order to streamline updating and drawing processes

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
     dt = 0
 
     while True:
@@ -15,9 +19,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        for game_obj in updatable:
+            game_obj.update(dt)
+
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+
+        for game_obj in drawable:
+            game_obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
